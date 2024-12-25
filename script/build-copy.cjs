47b3db1a6880ef30165683c5b153799e4129e7a0
zenoskongfu <zenos.chen@foxmail.com>
2024-12-25 17:49:32 +0800

    update config of building

---
+++ script/build-copy.cjs
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const distPath = path.resolve(__dirname, "../dist");
+
+const getFilePath = (filePath) => {
+	return path.resolve(__dirname, `../${filePath}`);
+};
+
+const getFileDistPath = (filePath) => {
+	return path.resolve(distPath, filePath);
+};
+
+const packageJsonPath = getFilePath("package.json");
+const readmePath = getFilePath("README.md");
+const licensePath = getFilePath("LICENSE");
+
+fs.cpSync(packageJsonPath, getFileDistPath("package.json"), { recursive: true });
+fs.cpSync(readmePath, getFileDistPath("README.md"), { recursive: true });
+fs.cpSync(licensePath, getFileDistPath("LICENSE"), { recursive: true });
