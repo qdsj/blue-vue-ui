9ad3d4fd8a25fddf2523054c88afd13f44e26d7c
zenoskongfu <zenos.chen@foxmail.com>
2024-12-25 23:12:02 +0800

    add new font-style

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -4,8 +4,9 @@ import { Button, BeiAnFooter } from "blue-vue-ui";
 
 <template>
 	<h1>App</h1>
-	<Button content="点击我"></Button>
-	<BeiAnFooter author="Zenos" github="https://github.com/blue-sky-123"></BeiAnFooter>
+	<Button class="pixel-zh" content="点击我"></Button>
+	<h2>点击我啊师大发大水啊师大发啥 赣 准备</h2>
+	<BeiAnFooter class=" pixel-zh" author="Zenos" github="https://github.com/blue-sky-123"></BeiAnFooter>
 </template>
 
 <style scoped lang="scss"></style>
--- src/BeiAnFooter/index.vue
+++ src/BeiAnFooter/index.vue
@@ -26,7 +26,7 @@ const props = defineProps<{
 	justify-content: center;
 	background-color: #f0f0f0;
 	color: #666;
-	font-size: 10px;
+	font-size: 12px;
 	line-height: 2;
 
 	padding: 1em;
